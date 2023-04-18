5211d347dc73b4c4b1d71cc7239f5d09db0117bd
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-04-18 12:41:42 +0200

    ajout champs inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,28 +5,65 @@ namespace App\Form;
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+            ->add('email', EmailType::class)
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('nom', TextType::class)
+            ->add('adresse1', TextType::class, [
+                'label' => 'Adresse principale'
+            ])
+            ->add('adresse2', TextType::class, [
+                'label' => 'Adresse secondaire',
+                'required' => false
+            ])
+            ->add('code_postal', IntegerType::class, [
+                'constraints' => new Length([
+                    'min' => 5,
+                    'max' => 5,
+                    'exactMessage' => 'Le code postal doit faire {{ limit }} chiffres'
+                    ])
+            ])
+            ->add('ville', TextType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Le mot de passe doit contenir {{ limit }} caractères minimum',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
                     ]),
                 ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer mot de passe'],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            /*->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
@@ -42,7 +79,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
+            ])*/
         ;
     }
 
